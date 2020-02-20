d7f313731df467fcff02ea00dcbb6d67b18d1cff
Payton <ptlohre@gmail.com>
2020-02-20 08:47:13 -0800

    started lesson 1
    lesson requirements:
    rotate camera
    focal point
    player
    player texture
    personal additions:
    hold space to increase speed
    some visual particle changes

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+
+{
+
+    private float rotSpeed = 50;
+    public float rotSpeedActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotSpeedActive = rotSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rotSpeedActive = rotSpeed * 3;
+        } else if (Input.GetKeyUp(KeyCode.Space)) {
+            rotSpeedActive = rotSpeed;
+        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotSpeedActive * Time.deltaTime);
+    }
+}
